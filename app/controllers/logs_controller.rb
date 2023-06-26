4e1b6c48a6873a83e9c0c0213ab65b25d9fe2642
saseihibino <132185275+saseihibino@users.noreply.github.com>
2023-06-26 15:53:37 +0900

    健康記録フォームの作成

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -9,4 +9,23 @@ class LogsController < ApplicationController
       end
     end
   end
+
+  def new
+    @log = Log.new
+  end
+
+  def create
+    @log = Log.new(log_params)
+    if @log.save?
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def log_params
+    params.require(:log).permit(:toilet, :meal, :mood, :walktime, :bodytemperature, :weight, :length, :memo).merge(user_id: current_user.id, pet_id: @selected_pet.id)
+  end
 end
